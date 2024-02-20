00be4a70a435fd56d678c3a932b7573ff58becfa
unknown <pawel.jaspinski@gmail.com>
2024-02-20 13:50:13 +0100

    New action generation code

--- src/actions.ts
+++ src/actions.ts
@@ -2,7 +2,6 @@ import {
     CompanionActionDefinition,
     CompanionActionDefinitions,
     CompanionActionEvent,
-    CompanionActionInfo,
     CompanionInputFieldCheckbox,
     CompanionInputFieldDropdown,
     CompanionInputFieldNumber,
@@ -11,39 +10,29 @@ import {
     InputValue,
     SomeCompanionActionInputField,
 } from "@companion-module/base";
-import {
-    ComandTemplateGroup,
-    CommandParameter,
-    CommandParameterOption,
-    CommandParameterProperty,
-    CommandParameterType,
-    CommandTemplate,
-} from "./types/commandsTemplates";
 import { CompanionCommonCallbackContext } from "@companion-module/base/dist/module-api/common";
-import { commandParameterTypeToInputType, generateMessageId, setValueAtPath, transformToTitleCase } from "./utils";
+import {
+    commandParameterTypeToInputType,
+    generateMessageId,
+    setValueAtPath,
+    transformDotCaseToTitleCase,
+} from "./utils";
 import StreamStudioInstance from "./index";
-import { Options } from "./types/options";
-import { getBooleanState } from "./projectState";
-import { RequestType } from "./types/requests";
+import { Option, Options } from "./types/options";
+import { Parameter, ParameterProperty, ParameterType, Request, RequestMethod } from "./types/apiDefinition";
+
+const COMMANDS_CATEGORY_NAME = "commands";
 
 const COMMAND_PARMS_TYPES_WITHOUT_OPTIONS_TO_GET = [
-    CommandParameterType.BOOLEAN,
-    CommandParameterType.STRING,
-    CommandParameterType.CONST,
-    CommandParameterType.NUMBER,
-    CommandParameterType.SELECT,
+    ParameterType.BOOLEAN,
+    ParameterType.STRING,
+    ParameterType.CONST,
+    ParameterType.NUMBER,
+    ParameterType.SELECT,
 ];
 
-// Only when value is stored in module's projectState then it can be used as toggle
-const BOOLEAN_CONTROLLABLE_TOPICS_WITH_STORED_VALUES = [
-    "SetDirectEditScene/active",
-    "SetDirectEditLayer/active",
-    "SetProgramMuted/muted",
-    "audiomixer.output.muted/muted",
-];
-
-export const getParameterTopic = (commandId: string, paramId: string) => {
-    return `${commandId}/${paramId}`;
+export const getParameterTopic = (requestType: string, paramId: string) => {
+    return `${requestType}/${paramId}`;
 };
 
 const DEFAULT_CHOICE_ID = "default_option";
@@ -52,21 +41,21 @@ const defaultChoice: DropdownChoice = {
     label: "Select an option...",
 };
 
-export const convertParamOptionsToChoices = (options: CommandParameterOption[] | string[]): DropdownChoice[] => {
+export const convertParamOptionsToChoices = (options: Option[] | string[]): DropdownChoice[] => {
     if (options.length === 0) return [defaultChoice];
     if (typeof options[0] === "string") {
         return [
             defaultChoice,
             ...options.map((option) => {
                 const stringOption = option as string;
-                return { id: stringOption, label: transformToTitleCase(stringOption) };
+                return { id: stringOption, label: stringOption };
             }),
         ];
     }
     return [
         defaultChoice,
         ...options.map((option) => {
-            const typedOption = option as CommandParameterOption;
+            const typedOption = option as Option;
             return {
                 id: typeof typedOption.id === "undefined" ? "undefined" : typedOption.id,
                 label: typedOption.value,
@@ -75,29 +64,29 @@ export const convertParamOptionsToChoices = (options: CommandParameterOption[] |
     ];
 };
 
-const getChoices = (param: CommandParameter, commandId: string, availableOptions: Options): DropdownChoice[] => {
-    if (param.type === CommandParameterType.SELECT && param.options) return convertParamOptionsToChoices(param.options);
-    const topic = getParameterTopic(commandId, param.id);
+const getChoices = (param: Parameter, requestType: string, availableOptions: Options): DropdownChoice[] => {
+    if (param.type === ParameterType.SELECT && param.values) return convertParamOptionsToChoices(param.values);
+    const topic = getParameterTopic(requestType, param.id);
     if (typeof availableOptions[topic] !== "undefined") return convertParamOptionsToChoices(availableOptions[topic]);
     return [{ id: 0, label: "No options available." }];
 };
 
 const getInput = (
-    param: CommandParameter,
-    template: CommandTemplate,
+    param: Parameter,
+    setRequest: Request,
     ssInstance: StreamStudioInstance
 ): SomeCompanionActionInputField => {
     const inputType = commandParameterTypeToInputType(param.type);
     switch (inputType) {
         case "number": {
             const label = param.range
-                ? `${param.description} (min ${param.range.min}, max ${param.range.max})`
-                : param.description;
+                ? `${param.prettyName} (min ${param.range.min}, max ${param.range.max})`
+                : param.prettyName;
             const input: CompanionInputFieldNumber = {
                 type: "number",
                 id: param.id,
                 label,
-                default: typeof param.value === "number" ? param.value : 0,
+                default: typeof param.defaultValue === "number" ? param.defaultValue : 0,
                 min: param.range?.min || 0,
                 max: param.range?.max || 0,
             };
@@ -107,8 +96,8 @@ const getInput = (
             const input: CompanionInputFieldTextInput = {
                 type: "textinput",
                 id: param.id,
-                label: param.description,
-                default: typeof param.value === "string" ? param.value : undefined,
+                label: param.prettyName,
+                default: typeof param.defaultValue === "string" ? param.defaultValue : undefined,
             };
             return input;
         }
@@ -116,19 +105,17 @@ const getInput = (
             const input: CompanionInputFieldCheckbox = {
                 type: "checkbox",
                 id: param.id,
-                label: param.description,
-                default: typeof param.value === "boolean" ? param.value : false,
+                label: param.prettyName,
+                default: typeof param.defaultValue === "boolean" ? param.defaultValue : false,
             };
             return input;
         }
         case "dropdown": {
-            const choices = getChoices(param, template.id, ssInstance.options);
+            const choices = getChoices(param, setRequest.type, ssInstance.options);
             const input: CompanionInputFieldDropdown = {
                 type: "dropdown",
                 id: param.id,
-                label: `${param.description}${
-                    param.property === CommandParameterProperty.REQUIRED ? " (required)" : ""
-                }`,
+                label: `${param.prettyName}${param.property === ParameterProperty.REQUIRED ? " (required)" : ""}`,
                 choices,
                 default: DEFAULT_CHOICE_ID,
             };
@@ -137,38 +124,34 @@ const getInput = (
     }
 };
 
-const getCallback = (template: CommandTemplate, ssInstance: StreamStudioInstance) => {
+const getCallback = (request: Request, ssInstance: StreamStudioInstance) => {
     return (action: CompanionActionEvent, _context: CompanionCommonCallbackContext) => {
         const messageId = generateMessageId();
         const message: any = {
-            "request-type": template.type,
+            "request-type": request.type,
             "message-id": messageId,
         };
 
         let requiredParamNotSet = false;
 
-        template.parameters.forEach((param) => {
-            if (param.type === CommandParameterType.BOOLEAN) {
-                if (param.property !== CommandParameterProperty.CONTROLLABLE) {
+        request.requestParams.forEach((param) => {
+            if (param.type === ParameterType.BOOLEAN) {
+                if (param.property !== ParameterProperty.CONTROLLABLE) {
                     return;
                 }
-                const paremterTopic = getParameterTopic(template.id, param.id);
-                if (BOOLEAN_CONTROLLABLE_TOPICS_WITH_STORED_VALUES.includes(paremterTopic)) {
-                    const value = !getBooleanState(paremterTopic, action.options, ssInstance.projectState);
-                    message[param.id] = value;
-                    return;
-                }
+                // const paremterTopic = getParameterTopic(request.type, param.id);
+                // get stored value
+                /* Logic for controllable boolean values goes here - we have to listen to notifications for them */
             }
 
             let value = action.options[param.id] as InputValue;
             if (typeof value === "undefined") {
                 // Handling required const parameters
-                if (param.type !== CommandParameterType.CONST || param.property !== CommandParameterProperty.REQUIRED)
-                    return;
-                if (param.value) value = param.value;
+                if (param.type !== ParameterType.CONST || param.property !== ParameterProperty.REQUIRED) return;
+                if (param.defaultValue) value = param.defaultValue;
             }
             if (value === DEFAULT_CHOICE_ID) {
-                if (param.property === CommandParameterProperty.REQUIRED) {
+                if (param.property === ParameterProperty.REQUIRED) {
                     requiredParamNotSet = true;
                 }
                 return;
@@ -180,7 +163,7 @@ const getCallback = (template: CommandTemplate, ssInstance: StreamStudioInstance
         if (requiredParamNotSet) {
             ssInstance.log(
                 "error",
-                `Executing ${template.id} ${template.description} command aborted because not all required parameters are set.`
+                `Executing ${request.type}: command aborted because not all required parameters are set.`
             );
         }
 
@@ -188,60 +171,45 @@ const getCallback = (template: CommandTemplate, ssInstance: StreamStudioInstance
     };
 };
 
-const generateActions = (
-    commandGroups: ComandTemplateGroup[],
-    ssInstance: StreamStudioInstance
-): CompanionActionDefinitions => {
+const generateActions = (ssInstance: StreamStudioInstance): CompanionActionDefinitions => {
     const actions: CompanionActionDefinitions = {};
-    commandGroups.forEach((group) => {
-        group.templates.forEach((template) => {
-            const groupName = transformToTitleCase(group.name);
+    const { apiDefinition } = ssInstance;
+    if (!apiDefinition) return actions;
+    apiDefinition.categories.forEach((category) => {
+        if (category.name === COMMANDS_CATEGORY_NAME) return;
+        category.requestGroups.forEach((group) => {
+            const groupName = transformDotCaseToTitleCase(group.name);
+
+            const setRequest = group.requests.find((request) => request.method === RequestMethod.SET);
+            if (!setRequest) return;
 
             const options: SomeCompanionActionInputField[] = [];
-            template.parameters.forEach((param) => {
-                if (param.type === CommandParameterType.CONST) return;
-                if (
-                    param.property === CommandParameterProperty.CONTROLLABLE &&
-                    param.type === CommandParameterType.BOOLEAN &&
-                    BOOLEAN_CONTROLLABLE_TOPICS_WITH_STORED_VALUES.includes(getParameterTopic(template.id, param.id))
-                )
-                    return;
-                options.push(getInput(param, template, ssInstance));
+            setRequest.requestParams.forEach((param) => {
+                if (param.type === ParameterType.CONST) return;
+                if (param.property === ParameterProperty.CONTROLLABLE && param.type === ParameterType.BOOLEAN) return;
+                options.push(getInput(param, setRequest, ssInstance));
             });
 
             const action: CompanionActionDefinition = {
-                name: `${groupName}: ${template.description}`,
+                name: `${groupName}: ${setRequest.prettyName}`,
                 options: options,
-                callback: getCallback(template, ssInstance),
-                subscribe: (action: CompanionActionInfo) => {
-                    template.parameters.forEach((param) => {
-                        if (COMMAND_PARMS_TYPES_WITHOUT_OPTIONS_TO_GET.includes(param.type)) return;
-                        ssInstance.getOptions(template.id, param.id);
+                callback: getCallback(setRequest, ssInstance),
+                subscribe: () => {
+                    setRequest.requestParams.forEach((param) => {
+                        // jak jest boolean controllable
+                        // get and save value
+                        // listen for notifications
 
-                        switch (template.id) {
-                            case "SetProgramMuted": {
-                                ssInstance.addAwaitedRequest(RequestType.AUDIOMIXER_OUTPUT_MUTED, {
-                                    output: "Master",
-                                });
-                                break;
-                            }
-                            case "audiomixer.output.muted": {
-                                ssInstance.addAwaitedRequest(RequestType.AUDIOMIXER_OUTPUT_MUTED, {
-                                    output: action.options["output"],
-                                });
-                                break;
-                            }
-                            case "SetDirectEditScene":
-                            case "SetDirectEditLayer": {
-                                ssInstance.addAwaitedRequest(RequestType.GET_DIRECT_EDIT);
-                                break;
-                            }
-                        }
+                        if (COMMAND_PARMS_TYPES_WITHOUT_OPTIONS_TO_GET.includes(param.type)) return;
+                        ssInstance.getOptions(setRequest.type, param.id);
                     });
+                },
+                unsubscribe: () => {
+                    //stop listening for notification jak jest boolean controllable
                 },
             };
 
-            actions[template.id] = action;
+            actions[setRequest.type] = action;
         });
     });
 
